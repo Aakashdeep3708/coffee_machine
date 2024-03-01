52698afe025d1c0665c5f89c5c2781e8e49d3789
Aakash Deep <134379080+Aakashdeep3708@users.noreply.github.com>
2024-03-01 23:12:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 30,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 45,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 55,
+    }
+}
+
+resources = {
+    "water": 500,
+    "milk": 300,
+    "coffee": 100,
+}
+
